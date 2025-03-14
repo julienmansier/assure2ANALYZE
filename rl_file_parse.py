124a0c3d494ca953b66a368f6d1834282963bd8d
QWERTYphish <julien.mansier@gmail.com>
2025-03-14 17:44:49 +0000

    Malware is done

--- rl_file_parse.py
+++ rl_file_parse.py
@@ -4,18 +4,23 @@ import csv
 import re
 
 checkMalware = False
+checkSuspect = False
 vulnExists = True
 vulnThreshold=0
 behaviors=[]
 vulns = []
-findings=[]
+findings={}
 
 def process_config(json_file):
-    global checkMalware, vulnExists, vulnThreshold, behaviors
+    global checkMalware, checkSuspect, vulnExists, vulnThreshold, behaviors, findings
     try:
         data = json.load(json_file)
+        findings["config"]=data
+        findings["findings"]=[]
         print(f"Successfully loaded Config data")
+        
         checkMalware = data["malware"]
+        checkSuspect = data["suspect"]
         vulnExists=data["vulnExists"]
         vulnThreshold=data["vulnThreshold"]
         behaviors=data["behaviors"]
@@ -43,9 +48,12 @@ def process_vulns(vuln_file):
 
 
 
-def process_malware(json_file):
+def process_malware(report_file, malware_file):
+    global findings
+
     try:
-        data = json.load(json_file)
+        data = json.load(report_file)
+        mal = json.load(malware_file)
         print(f"Successfully loaded the Report data")
     
         ##Check for Malware
@@ -56,20 +64,35 @@ def process_malware(json_file):
                 print("No malware detected!")
             else:
                 print("Malware Detected!")
+                
+                for item in mal:
+                    temp = {}
+                    if item["malware"]: ## Check if malware is TRUE
+                        temp["type"]="malware"
+                        temp["name"]=item["malwareName"]
+                        temp["location"]=item["detections"]
+                        findings["findings"].append(temp)
+                    elif  checkSuspect and  item["suspect"]: ## Check if config has suspect set to TRUE
+                        temp["type"]="suspected malware"
+                        temp["name"]=item["malwareName"]
+                        temp["location"]=item["detections"]
+                        findings["findings"].append(temp)
         else:
             print("Not checking for malware...")
-
 
     except json.JSONDecodeError:
         print(f"Error: Invalid JSON format in the Report file")
     except Exception as e:
         print(f"An error occurred while processing Report JSON: {str(e)}")
 
+
+
 def main():
     parser = argparse.ArgumentParser(description="Process needed files")
-    parser.add_argument('-f', '--file', type=argparse.FileType('r'), required=True, help="Input JSON file")
-    parser.add_argument('-c', '--config', type=argparse.FileType('r'), required=True, help="Config JSON file")
-    parser.add_argument('-v', '--vulns', type=argparse.FileType('r'), required=True, help="CVE JSON file")
+    parser.add_argument('-r', '--report', type=argparse.FileType('r'), required=True, help="Input Report JSON file")
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'), required=True, help="Input Config JSON file")
+    parser.add_argument('-v', '--vulns', type=argparse.FileType('r'), required=True, help="Input CVE JSON file")
+    parser.add_argument('-m', '--malware', type=argparse.FileType('r'), required=True, help="Input Malware JSON file")
     args = parser.parse_args()
 
         
@@ -80,11 +103,17 @@ def main():
     process_vulns(args.vulns)
     
     ## Process the report file
-    process_malware(args.file)
+    process_malware(args.report, args.malware)
 
 
-    args.file.close()
+    args.report.close()
     args.config.close()
+    args.vulns.close()
+    args.malware.close()
+
+    outfile = open("findings.json", "w")
+    outfile.write(json.dumps(findings))
+    outfile.close()
 
 if __name__ == "__main__":
     main()
